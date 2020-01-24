18ebd984114f78f3724f13bd477a80c42b6d214b
brendamrl816 <brendamrl816@gmail.com>
2020-01-23 21:13:45 -0500

    design changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,194 +3,169 @@
 @section('content')
 
 
+    <div id="preloader">
+      <div style="font-size:200%;">Brenda Castillo</div>
+      <div class="preloader-dot-container">
+        <div id="pl-dot1" class="loader-dot" style="background-color:#eab2bc;"></div>
+        <div id="pl-dot2" class="loader-dot" style="background-color:#80c5ac;"></div>
+        <div id="pl-dot3" class="loader-dot" style="background-color:#b890c5;"></div>
+        <div id="pl-dot4" class="loader-dot" style="background-color:#74abc5;"></div>
+        <div id="pl-dot5" class="loader-dot" style="background-color:#c7843f;"></div>
+        <div id="pl-dot6" class="loader-dot" style="background-color:#9fb361;"></div>
+      </div>
 
-     <button class="nav-button show"><i class="fa fa-bars" aria-hidden="true"></i></button>
-     <button class="close-nav hide"><i class="fa fa-times" aria-hidden="true"></i></button>
-        <div class="divMenu hide">
-            <ul id="myMenu">
-                <li data-menuanchor="home" class="active nav-li"><a class="nav-a" href="#home">HOME</a></li>
-                <li data-menuanchor="career" class="nav-li"><a class="nav-a" href="#career">CAREER</a></li>
-                <li data-menuanchor="personal" class="nav-li"><a class="nav-a" href="#personal">PERSONAL</a></li>
-                <li data-menuanchor="contact" class="nav-li"><a class="nav-a" href="#contact">CONTACT ME</a></li>
-            </ul>
+
+    </div>
+     <button class="nav-button hide" onclick="toggleNavButton(event)"><i class="fa fa-bars" aria-hidden="true"></i></button>
+
+      <div class="divMenu hide">
+          <ul id="myMenu">
+              <li data-menuanchor="home" class="active nav-li"><a class="nav-a" href="#home">HOME</a></li>
+              <li data-menuanchor="career" class="nav-li"><a class="nav-a" href="#career">PROJECTS</a></li>
+              <li data-menuanchor="personal" class="nav-li"><a class="nav-a" href="#personal">PERSONAL</a></li>
+              <li data-menuanchor="contact" class="nav-li"><a class="nav-a" href="#contact">CONTACT ME</a></li>
+          </ul>
+      </div>
+
+
+     <div id="fullpage">
+        <div class="section fp-auto-height">
+            <div class="enterDiv">
+                <div class="enterDiv-container-left">
+
+                </div>
+                <div class="enterDiv-container-right">
+                    <div class="enterDiv-container-right-pic">
+                    </div>
+                    <div class="enterDiv-container-right-content">
+                        <div class="enterDiv-container-right-content-text">
+                          <h1>Hello, welcome to Brenda Castillo's Portfolio</h1>
+                          <p>I am a software developer. Please take a look at my work, visit my social media,
+                            check out my resume and learn how to get in contact with me.
+                          </p>
+                          <p>Thanks for coming!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="arrow">
+                  <div>scroll down</div>
+                  <div><i class="fa fa-arrow-down" aria-hidden="true"></i></div>
+                </div>
+            </div>
+
+
         </div>
-        <!--<div style="width:100%; height:60px; background-color:#f2f2f2">&nbsp;</div>-->
-        <!--<div class="big-name">Brenda Castillo</div>-->
 
-         <div id="fullpage">
-            <div class="section">
-                <div class="homeMainDiv">
-                    <img  class="puzzle1" src="/pictures/puzzle1.png">
-                    <img class="puzzle2" src="/pictures/puzzle2.png">
-                    <img class="puzzle3" src="/pictures/puzzle3.png">
-                    <img class="puzzle4" src="/pictures/puzzle4.png">
-                    <div class="mainPic"></div>
-                </div>
-
-                <div class="enterDiv">
-                        <h1 class="h-move">Brenda Castillo</h1>
-                        <h2 class="h-move">Software Developer <span class="dot1">.</span><span class="dot2">.</span><span class="dot3">.</span></h2>
-                        <div class="arrow">
-                            <div>scroll down</div>
-                            <div><i class="fa fa-arrow-down" aria-hidden="true"></i></div>
+        <div class="section fp-auto-height">
+            <div class="projects-container">
+              <div style="margin: auto auto 20px auto; width:95%; text-align:center;">
+                <h2>PROJECTS</h2>
+              </div>
+              <div class="projects-container-left">
+                <p class="projects-container-left-text">This is a summary of the projects I've worked on.  Please
+                feel free to ask me about them</p>
+              </div>
+              <div class="projects-container-right">
+                  <div id="pcr-table">
+                    <div id="gmplanner-pic-container" class="pcr-table-cell">
+                      <a href="http://gmplanner.webbmo.com">
+                        <div class="pcr-table-cell-dark">
+                          <div style="text-align:center;color:white; margin-top:30%;">
+                            <i class="fa fa-eye pcr-table-cell-dark-i"></i>
+                            gmPlanner
+                          </div>
                         </div>
-                </div>
-
-
-            </div>
-
-            <div class="section fp-auto-height">
-                <div class="main-container opacity8">
-                    <div class="head2 slide-left"><span class="inside-header">WORK</span></div>
-                     <div class="insideMain blue">
-                         <br>
-                         <div class="lelement bold">Samsung</div>
-                         <div class="relement bold">March 2015 - May 2018</div>
-                         <br>
-                         <p style="margin:20px">
-                           Oversaw data returns for Samsung.  During my time there I wrote Excel VBA programs that auto-generate files for ease of access, email condensation, and request sequence creation.
-                           The programs were coded to automatically classify information entered from purchase tables which are derived from the database. Units are sorted according to model numbers, streamlining
-                           the process and cutting the labor time by 50%.
-                        <br>
-                            In addition, I was part of the Washer Project Recall, where I was tasked with creating uploade files for processing over 3,000 different requests from customers all over the US.  I
-                            also assited with discrepancy reports and implementing programs to compare the incoming daily reports.
-                         </p>
-                     </div>
-
-                     <div class="insideMain">
-                         <table style="text-align:center; width:90%; margin-left:5%">
-                            <tr>
-                                <th>Techonology Summary:</th>
-                                <td style="text-align:left">C++, Java, PHP, JavaScript, JQuery, MySQL, CSS, HTML5, AngularJS, Laravel</td>
-                            </tr>
-                        </table>
-                     </div>
-
-                    <div class="head2 slide-left"><span class="inside-header">PROJECTS</span></div>
-                    <div class="insideMain green">
-
-                        <div class="bold" style="width:100%; text-align:center; margin-top:15px; margin-bottom:20px">
-                            <a style="text-decoration:none; color:inherit; cursor:pointer" href="http://gmplanner.com">
-                                <img style="width:30%" src="/pictures/gmplanner.jpg">
-                                <div style="display:block">GMPlanner</div>
-                            </a>
-                        </div>
-
-                        <ul>
-                            <li>Application backend implemented with Laravel.</li>
-                            <li>User data stored in MySQL server</li>
-                            <li>Implemented API of application and flagged user events based on dates and repetitions.</li>
-                            <li>Made use of Laravel middleware resources for data authentication.</li>
-                            <li>User Interface implemented with AngularJS.</li>
-                            <li>Used promises for getting/updating/deleting user information from backend.</li>
-                            <li>Extensively utilized angular resources such as controllers, directives and services for data input and output.</li>
-                            <li>Different types of calendar displays were handled by Angular's ui-route.</li>
-                             <li>Custom Angular's parsers and formatters for input validation.</li>
-                            <li>Changed layout display according to screen size with CSS media queries and animations.</li>
-                            <li>Application server deployed on Amazon Linux Server.</li>
-                            <li>Continuous integration using GIT</li>
-                        </ul>
-
+                      </a>
+                        <img class="pcr-table-cell-container" src="/pictures/gmplanner.png">
                     </div>
-
-                    <!--<div class="head2 slide-left"><span class="inside-header">EDUCATION</span></div>-->
-                    <!--<h2 style="width:100%; text-align:left; margin-top:10px" class=""><span style="margin-left:5%">EDUCATION</span></h2>-->
-                    <!--<div class="insideMain brown" style="margin-bottom:30px">-->
-                    <!--     <div class="lelement bold">Bergen Community College</div>-->
-                    <!--     <div class="relement italic">Computer Science</div>-->
-                    <!--     <br>-->
-                    <!--     <div class="lelement">Associates in Science</div>-->
-                    <!--     <br>-->
-                    <!--     <div class="lelement bold">New Jersey Institute of Technology</div>-->
-                    <!--     <div class="relement italic">Computer Science</div>-->
-                    <!--     <br>-->
-                    <!--     <div class="lelement">Currently attending</div>-->
-                    <!--</div>-->
-                </div>
-            </div>
-
-
-
-            <div class="section fp-auto-height">
-
-                <div class="slideRelative">
-                    <div class="SlidePic"></div>
-
-                </div>
-
-                <div class="main-container">
-                    <div class="insideMain">
-                        <div style="background-color:white; display:inline-block; padding:10px">
-                            <p style="margin-left:5%; margin-right:5%; margin-bottom:40px">
-                            My personal modus operandi is:  I believe obstacles, no matter how insurmountable they may appear, can be traversed through perseverance and dedication.
-                            I commit whole heartily to everything I undertake, including helping others at my own expense, no matter the personal cost. I believe in justice and fairness,
-                            and paying knowledge forward by teaching others what I have learned.
-                            </p>
-
-                        </div>
-                            <div class="head4 animate slide-left"><span class="inside-header">My Favorite Things</span></div>
-                            <div class="insideMain pink">
-                                <ul class="">
-                                    <li>Color: Peach</li>
-                                    <li>Sport: Soccer</li>
-                                    <li>Sports team: Barcelona FC</li>
-                                    <li>Movie: Saving Private Ryan/Grease</li>
-                                    <li>Animal: Wolf/Owl</li>
-                                </ul>
+                    <div id="itp-pic-container" class="pcr-table-cell">
+                      <a href="http://itp.webbmo.com/">
+                          <div class="pcr-table-cell-dark">
+                            <div style="text-align:center;color:white; margin-top:30%;">
+                              <i class="fa fa-eye pcr-table-cell-dark-i"></i>
+                              itpUSA
                             </div>
-
+                          </div>
+                        </a>
+                        <img class="pcr-table-cell-container" src="/pictures/itp.png">
                     </div>
-
-                </div>
-            </div>
-
-            <div class="section fp-auto-height">
-                <div class="main-container opacity4">
-                    <!--<div class="insideMain">-->
-                    <h1>CONTACT ME</h1>
-                        <div class="m-top link"><span class="bold head3">Email: </span>&nbsp;brendamrl816@gmail.com</div>
-                        <br>
-                        <div class="link"><span class="bold head3">LinkedIn: </span><a style="text-decoration:none; color:inherit; cursor:pointer" href="https://www.linkedin.com/in/brenda-castillo-06450934">&nbsp; linkedin.com/in/brenda-castillo-06450934</a></div>
-
-                        <div style="width:100%">
-                           @if (session('status'))
-                                <div class="email-status">
-                                    {{ session('status') }}
-                                </div>
-                            @endif
-                            @foreach ($errors->all() as $error)
-                                <p class="email-error">*{{$error}}</p>
-                            @endforeach
+                    <div id="comming_soon-pic-container" class="pcr-table-cell">
+                      <div class="pcr-table-cell-dark">
+                        <div style="text-align:center;color:white; margin-top:30%;">
+                          <i class="fa fa-eye-slash pcr-table-cell-dark-i"></i>
                         </div>
-
-                        <form class="contactForm" method="post" action="/sendEmail">
-                            {!! csrf_field() !!}
-
-                            <div class="message">
-                                <div class="m-name">
-                                    <input class="message-text" type="text" placeholder="your name" name="name"  value="{{ old('name') }}" required>
-                                </div>
-                                <div class="m-email">
-                                    <input class="message-text" type="email" placeholder="your email" name="email"  value="{{ old('email') }}" required>
-                                </div>
-                            </div>
-
-
-                            <div class="message">
-                                <textarea class="message-textArea" placeholder="message" name="content"  value="{{ old('content') }}" required></textarea>
-                            </div>
-                            <div class="message" style="margin-top:10px; text-align:center">
-                                <button class="mbutton">send message</button>
-                            </div>
-
-                        </form>
-
-                    <!--</div>-->
-
-                </div>
+                      </div>
+                        <img class="pcr-table-cell-container" src="/pictures/comming_soon.png">
+                    </div>
+                  </div>
+              </div>
             </div>
 
         </div>
+
+
+
+        <div class="section fp-auto-height">
+          <!-- <div style="margin: auto auto 20px auto; width:95%; text-align:center;">
+            <h2>PERSONAL INFORMATION</h2>
+          </div> -->
+            <div class="personal">
+                <div class="personal-pic"></div>
+                <div class="personal-container">
+                    <div class="personal-container-text">
+                      <p>My personal modus operandi is:  I believe obstacles, no matter how insurmountable they may appear, can be traversed through perseverance and dedication.
+                      I commit whole heartily to everything I undertake, including helping others. I believe in justice and fairness,
+                      I believe God gives strength to his people when they need it and blesses them greatly.
+                      </p>
+                      <p>
+                        Doggie lover who also enjoys dancing Marinera Norteña.  Being a girl in a male dominated field has helped me grow professionally.
+                        My favorite color is peach. I am a huge soccer fan and my favorite sports team is Football Club Barcelona.  I love spending time with
+                        family and friends, and always try to find the time to see them.  I lived in the US for 20 years and recently moved to Peru, still adapting to the cultural differences but definitely worth the change.
+                      </p>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="section fp-auto-height">
+          <div class="contact">
+            <div style="margin: 8% auto 5% auto; width:95%; text-align:center;">
+              <h2>CONTACT ME</h2>
+            </div>
+            <div class="contact-container">
+              <div class="contact-icons">
+                <a href="https://www.linkedin.com/in/brenda-castillo-illescas-18143a167">
+                  <i class="fa fa-linkedin"></i>
+                </a>
+                <div class="contact-icons-text">LinkedIn</div>
+              </div>
+              <div class="contact-icons">
+                <a href="https://www.instagram.com/bmci_816">
+                  <i class="fa fa-instagram"></i>
+                </a>
+                <div class="contact-icons-text">Instagram</div>
+              </div>
+              <div class="contact-icons">
+                <a href="mailto:brendamrl816@gmail.com">
+                  <i class="fa fa-envelope"></i>
+                </a>
+                <div class="contact-icons-text">Email</div>
+              </div>
+            </div>
+
+            <div class="resume-container">
+              <!-- <iframe src="url=../pictures/BrendaCastillo_Resume.pdf&embedded=true"></iframe> -->
+              <img class="resume-container-pdf" src="pictures/BrendaCastillo_Resume-1.png"><img>
+              <img class="resume-container-pdf" src="pictures/BrendaCastillo_Resume-2.png"><img>
+
+            </div>
+          </div>
+        </div>
+
+
+    </div>
 
 
 
